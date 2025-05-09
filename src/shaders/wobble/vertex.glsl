6d1d9f44a30f8503f57cf1d3af62e64a1a49900e
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-09 17:17:33 +0100

    colors

--- src/shaders/wobble/vertex.glsl
+++ src/shaders/wobble/vertex.glsl
@@ -9,6 +9,8 @@ uniform float uWarpPositionFrequency;
 uniform float uWarpTimeFrequency;
 uniform float uWarpStrength;
 
+varying float vWobble;
+
 #include ../includes/simplexNoise4d.glsl
 
 float getWobble (vec3 position) {
@@ -46,4 +48,7 @@ void main() {
     vec3 toB = normalize(positionB - csm_Position);
 
     csm_Normal = cross(toA, toB);
+
+    // Varyings
+    vWobble = wobble / uStrength;
 }
